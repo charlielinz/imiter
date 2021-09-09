90b8649e35417495d2c5edc8991a543a1901cbea
charlielinz <ilovealinlin@gmail.com>
2021-09-09 15:12:47 +0800

    customized element selector

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
 import unittest
 from secrets import ACCOUNT
-from time import sleep
 
 import HtmlTestRunner
 
 from utils import ANDROID_SETTING_CAPS, CS_APP_CAPS, GOOGLE_STORE_CAPS
-from users import CSUser, DriverManager
+from users import User, CSUser
 from xpaths import ADVENTUREPAGE, ANDROID_SETTING, HOMEPAGE, SIGNUPPAGE
 
 
 class TestGeneralSignin(unittest.TestCase):
     def setUp(self):
-        self.manager = DriverManager(CS_APP_CAPS)
-        self.cs_user = CSUser(self.manager)
+        self.user = User(CS_APP_CAPS)
+        self.cs_user = CSUser(self.user)
 
     def tearDown(self):
-        self.manager.driver.quit()
+        self.user.driver.quit()
 
     def test_sign_up(self):
         self.cs_user.sign_up(
@@ -25,65 +24,63 @@ class TestGeneralSignin(unittest.TestCase):
             password="00000000",
             password_confirmed="00000000",
         )
-        sign_up_finish = self.cs_user._find_element_by_xpath(
+        sign_up_finish = self.cs_user.element_selector(
             SIGNUPPAGE["interested_theme_finish"]
         )
         self.assertEqual(sign_up_finish.get_attribute("clickable"), "true")
 
     def test_sign_in(self):
         self.cs_user.sign_in("test2@test.com", "00000000")
-        account_page = self.cs_user._find_element_by_xpath(HOMEPAGE["account"])
+        account_page = self.cs_user.element_selector(HOMEPAGE["account"])
         self.assertEqual(account_page.get_attribute("clickable"), "true")
 
 
 class TestThirdPartySignin(unittest.TestCase):
     def setUp(self):
-        self.manager = DriverManager(CS_APP_CAPS)
-        self.cs_user = CSUser(self.manager)
+        self.user = User(CS_APP_CAPS)
+        self.cs_user = CSUser(self.user)
 
     def tearDown(self):
-        self.manager.driver.quit()
+        self.user.driver.quit()
 
     def test_1_clear_storage(self):
-        self.manager = DriverManager(ANDROID_SETTING_CAPS)
-        self.cs_user = CSUser(self.manager)
+        self.user = User(ANDROID_SETTING_CAPS)
+        self.cs_user = CSUser(self.user)
         self.cs_user.clear_chrome_storage()
-        self.manager = DriverManager(GOOGLE_STORE_CAPS)
-        self.cs_user = CSUser(self.manager)
+        self.user = User(GOOGLE_STORE_CAPS)
+        self.cs_user = CSUser(self.user)
         self.cs_user.clear_google_storage()
 
     def test_facebook_sign_in(self):
         self.cs_user.facebook_sign_in(
             email=ACCOUNT["facebook_email"], password=ACCOUNT["facebook_password"]
         )
-        account_page = self.cs_user._find_element_by_xpath(HOMEPAGE["account"])
+        account_page = self.cs_user.element_selector(HOMEPAGE["account"])
         self.assertEqual(account_page.get_attribute("clickable"), "true")
 
     def test_line_sign_in(self):
         self.cs_user.line_sign_in(
             email=ACCOUNT["line_email"], password=ACCOUNT["line_password"]
         )
-        account_page = self.cs_user._find_element_by_xpath(HOMEPAGE["account"])
+        account_page = self.cs_user.element_selector(HOMEPAGE["account"])
         self.assertEqual(account_page.get_attribute("clickable"), "true")
 
     def test_google_sign_in(self):
         self.cs_user.google_sign_in(
             email=ACCOUNT["google_email"], password=ACCOUNT["google_password"]
         )
-        account_page = self.cs_user._find_element_by_xpath(HOMEPAGE["account"])
+        account_page = self.cs_user.element_selector(HOMEPAGE["account"])
         self.assertEqual(account_page.get_attribute("clickable"), "true")
 
 
 class TestAdventure(unittest.TestCase):
     def setUp(self):
-        self.manager = DriverManager(CS_APP_CAPS)
-        self.cs_user = CSUser(self.manager)
+        self.user = User(CS_APP_CAPS)
+        self.cs_user = CSUser(self.user)
 
     def test_early_bird(self):
         self.cs_user.early_bird_list()
-        early_bird_title = self.cs_user._find_element_by_class_and_text(
-            className="android.widget.TextView", text="早鳥首賣"
-        )
+        early_bird_title = self.cs_user.element_selector("+android.widget.TextView+早鳥首賣")
         self.assertEqual(early_bird_title.get_attribute("text"), "早鳥首賣")
 
     def test_early_bird_product_page(self):
--- users.py
+++ users.py
@@ -16,177 +16,194 @@ from xpaths import (
 )
 
 
-class DriverManager:
+class User:
+    """
+    this is a mobile user
+    """
+
     def __init__(self, desired_cap):
         self.desired_cap = desired_cap
-        self._remote()
-
-    def _remote(self):
         self.driver = webdriver.Remote("http://localhost:4723/wd/hub", self.desired_cap)
         self.driver.implicitly_wait(10)
 
-
-class User:
     def element_selector(self, string):
-        pass
+        """
+        customized element selector- select element according to the prefix of string
+        the given string needs to start with a prefix, the rules are below:
+            "@" = by id
+            "#" = by class
+            "/" = by xpath
+            "+" = by class & text
+        """
+        prefix = string[:1]
+        if prefix == "@":
+            id_ = string.replace("@", "", 1)
+            return self.user.driver.find_element_by_id(id_)
+        if prefix == "#":
+            class_ = string.replace("#", "", 1)
+            return self.user.driver.find_element_by_class_name(class_)
+        if prefix == "/":
+            xpath = string
+            return self.user.driver.find_element_by_xpath(xpath)
+        if prefix == "+":
+            original_string = string.replace("+", "", 1)
+            attribute_list = original_string.split("+")
+            class_name = attribute_list[0]
+            text = attribute_list[1]
+            return self.user.driver.find_element_by_android_uiautomator(
+                f".className('{class_name}').text('{text}')"
+            )
+        raise ValueError("the string doesn't include a prefix")
 
-    def _find_element_by_id(self, id_):
-        return self.manager.driver.find_element_by_id(id_)
-
-    def _find_element_by_id_and_click(self, id_):
-        self.manager.driver.find_element_by_id(id_).click()
-
-    def _find_element_by_id_and_send_keys(self, id_, key):
-        self.manager.driver.find_element_by_id(id_).send_keys(key)
-
-    def _find_element_by_xpath(self, xpath):
-        return self.manager.driver.find_element_by_xpath(xpath)
-
-    def _find_element_by_xpath_and_click(self, xpath):
-        self.manager.driver.find_element_by_xpath(xpath).click()
-
-    def _find_element_by_xpath_and_send_keys(self, xpath, key):
-        self.manager.driver.find_element_by_xpath(xpath).send_keys(key)
-
-    def _find_element_by_classname(self, className):
-        return self.manager.driver.find_element_by_class_name(className)
-
-    def _find_element_by_class_and_text(self, className, text):
-        return self.manager.driver.find_element_by_android_uiautomator(
-            f'.className("{className}").text("{text}")'
-        )
-
-    def _find_element_by_class_and_text_and_click(self, className, text):
-        self._find_element_by_class_and_text(className, text).click()
-
-    def _find_elements_by_id(self, id_):
-        return self.manager.driver.find_elements_by_id(id_)
-
-    def _find_elements_by_classname(self, className):
-        return self.manager.driver.find_elements_by_class_name(className)
+    def elements_selector(self, string):
+        """
+        customized element selector- select elements according to the prefix of string
+        the given string needs to start with a prefix, the rules are below:
+            "@" = by id
+            "#" = by class
+            "/" = by xpath
+            "+" = by class & text
+        """
+        prefix = string[:1]
+        if prefix == "@":
+            id_ = string.replace("@", "", 1)
+            return self.user.driver.find_elements_by_id(id_)
+        if prefix == "#":
+            class_ = string.replace("#", "", 1)
+            return self.user.driver.find_elements_by_class_name(class_)
+        if prefix == "/":
+            xpath = string
+            return self.user.driver.find_elements_by_xpath(xpath)
+        if prefix == "+":
+            original_string = string.replace("+", "", 1)
+            attribute_list = original_string.split("+")
+            class_name = attribute_list[0]
+            text = attribute_list[1]
+            return self.user.driver.find_elements_by_android_uiautomator(
+                f".className('{class_name}').text('{text}')"
+            )
+        raise ValueError("the string doesn't include a prefix")
 
     def _swipe_up(self):
-        screen_width = self.manager.driver.get_window_size()["width"]
-        screen_height = self.manager.driver.get_window_size()["height"]
+        screen_width = self.user.driver.get_window_size()["width"]
+        screen_height = self.user.driver.get_window_size()["height"]
         start_point = [screen_width * 0.5, screen_height * 0.75]
         end_point = [screen_width * 0.5, screen_height * 0.25]
-        self.manager.driver.swipe(
+        self.user.driver.swipe(
             start_point[0], start_point[1], end_point[0], end_point[1]
         )
 
     def _swipe_down(self):
-        screen_width = self.manager.driver.get_window_size()["width"]
-        screen_height = self.manager.driver.get_window_size()["height"]
+        screen_width = self.user.driver.get_window_size()["width"]
+        screen_height = self.user.driver.get_window_size()["height"]
         start_point = [screen_width * 0.5, screen_height * 0.25]
         end_point = [screen_width * 0.5, screen_height * 0.75]
-        self.manager.driver.swipe(
+        self.user.driver.swipe(
             start_point[0], start_point[1], end_point[0], end_point[1]
         )
 
     def _swipe_left(self):
-        screen_width = self.manager.driver.get_window_size()["width"]
-        screen_height = self.manager.driver.get_window_size()["height"]
+        screen_width = self.user.driver.get_window_size()["width"]
+        screen_height = self.user.driver.get_window_size()["height"]
         start_point = [screen_width * 0.75, screen_height * 0.5]
         end_point = [screen_width * 0.25, screen_height * 0.5]
-        self.manager.driver.swipe(
+        self.user.driver.swipe(
             start_point[0], start_point[1], end_point[0], end_point[1]
         )
 
     def _swipe_right(self):
-        screen_width = self.manager.driver.get_window_size()["width"]
-        screen_height = self.manager.driver.get_window_size()["height"]
+        screen_width = self.user.driver.get_window_size()["width"]
+        screen_height = self.user.driver.get_window_size()["height"]
         start_point = [screen_width * 0.25, screen_height * 0.5]
         end_point = [screen_width * 0.75, screen_height * 0.5]
-        self.manager.driver.swipe(
+        self.user.driver.swipe(
             start_point[0], start_point[1], end_point[0], end_point[1]
         )
 
 
 class CSUser(User):
-    def __init__(self, manager):
-        self.manager = manager
+    """
+    this is a CS app user
+    """
+
+    def __init__(self, user):
+        self.user = user
 
     def adventure_page(self):
-        self._find_element_by_xpath_and_click(HOMEPAGE["adventure"])
+        self.element_selector(HOMEPAGE["adventure"]).click()
 
     def search_page(self):
-        self._find_element_by_xpath_and_click(HOMEPAGE["search"])
+        self.element_selector(HOMEPAGE["search"]).click()
 
     def favorite_page(self):
-        self._find_element_by_xpath_and_click(HOMEPAGE["favorite"])
+        self.element_selector(HOMEPAGE["favorite"]).click()
 
     def account_page(self):
-        self._find_element_by_xpath_and_click(HOMEPAGE["account"])
+        self.element_selector(HOMEPAGE["account"]).click()
 
     def early_bird_list(self):
         while True:
             try:
-                self._find_element_by_class_and_text_and_click(
-                    className="android.widget.TextView", text="看全部早鳥首賣"
-                )
+                self.element_selector("+android.widget.TextView+看全部早鳥首賣")
                 break
             except NoSuchElementException:
                 self._swipe_up()
 
-    def early_bird_prudoct_page(self):
-        self.early_bird_list()
-        for looking_early_product in range(20):
-            button_list = self._find_elements_by_classname("android.widget.TextView")
-            for button in button_list:
-                if "金額" in str(button.get_attribute("text")):
-                    button.click()
+    # def early_bird_prudoct_page(self):
+    #     self.early_bird_list()
+    #     for looking_early_product in range(20):
+    #         button_list = self._find_elements_by_classname("android.widget.TextView")
+    #         for button in button_list:
+    #             if "金額" in str(button.get_attribute("text")):
+    #                 button.click()
 
     def sign_up_page(self):
         self.account_page()
-        self._find_element_by_xpath_and_click(ACCOUNTPAGE["sign_up_page"])
+        self.element_selector(ACCOUNTPAGE["sign_up_page"]).click()
 
     def sign_in_page(self):
         self.account_page()
-        self._find_element_by_xpath_and_click(ACCOUNTPAGE["sign_in_normal"])
+        self.element_selector(ACCOUNTPAGE["sign_in_normal"]).click()
 
     def sign_up(self, lastname, firstname, email, password, password_confirmed):
         self.sign_up_page()
-        self._find_element_by_xpath_and_send_keys(SIGNUPPAGE["last_name"], lastname)
-        self._find_element_by_xpath_and_send_keys(SIGNUPPAGE["first_name"], firstname)
-        self._find_element_by_xpath_and_send_keys(SIGNUPPAGE["email"], email)
-        self._find_element_by_xpath_and_send_keys(SIGNUPPAGE["password"], password)
-        self._find_element_by_xpath_and_send_keys(
-            SIGNUPPAGE["password_confirmed"], password_confirmed
+        self.element_selector(SIGNUPPAGE["last_name"]).send_keys(lastname)
+        self.element_selector(SIGNUPPAGE["first_name"]).send_keys(firstname)
+        self.element_selector(SIGNUPPAGE["email"]).send_keys(email)
+        self.element_selector(SIGNUPPAGE["password"]).send_keys(password)
+        self.element_selector(SIGNUPPAGE["password_confirmed"]).send_keys(
+            password_confirmed
         )
-        self._find_element_by_xpath_and_click(SIGNUPPAGE["sign_up"])
+        self.element_selector(SIGNUPPAGE["sign_up"]).click()
 
     def sign_in(self, email, password):
         self.sign_in_page()
-        self._find_element_by_xpath_and_send_keys(SIGNINPAGE["email"], email)
-        self._find_element_by_xpath_and_send_keys(SIGNINPAGE["password"], password)
-        self._find_element_by_xpath_and_click(SIGNINPAGE["sign_in"])
+        self.element_selector(SIGNINPAGE["email"]).send_keys(email)
+        self.element_selector(SIGNINPAGE["password"]).send_keys(password)
+        self.element_selector(SIGNINPAGE["sign_in"]).click()
 
     def clear_chrome_storage(self):
-        self._find_element_by_xpath_and_click(ANDROID_SETTING["app & notifications"])
-        self._find_element_by_id_and_click("com.android.settings:id/header_details")
+        self.element_selector(ANDROID_SETTING["app & notifications"]).click()
+        self.element_selector("@com.android.settings:id/header_details").click()
         sleep(1)
-        app_list = self._find_elements_by_id("android:id/title")
+        app_list = self.elements_selector("@android:id/title")
         for app in app_list:
             app_name = app.get_attribute("text")
             if "Chrome" in str(app_name):
                 app.click()
                 sleep(1)
-                self._find_element_by_xpath_and_click(
-                    ANDROID_SETTING["Storage & Cache"]
-                )
-                self._find_element_by_xpath_and_click(ANDROID_SETTING["Clear Storage"])
-                self._find_element_by_xpath_and_click(
+                self.element_selector(ANDROID_SETTING["Storage & Cache"]).click()
+                self.element_selector(ANDROID_SETTING["Clear Storage"]).click()
+                self.element_selector(
                     ANDROID_SETTING["Google Chrome Clear All Data"]
-                )
-                self._find_element_by_xpath_and_click(
-                    ANDROID_SETTING["Google Chrome OK"]
-                )
+                ).click()
+                self.element_selector(ANDROID_SETTING["Google Chrome OK"]).click()
                 break
 
     def clear_google_storage(self):
         try:
-            storepage_button_list = self._find_elements_by_id(
-                "com.android.vending:id/0_resource_name_obfuscated"
+            storepage_button_list = self.elements_selector(
+                "@com.android.vending:id/0_resource_name_obfuscated"
             )
             for button in storepage_button_list:
                 button_content = button.get_attribute("content-desc")
@@ -194,8 +211,8 @@ class CSUser(User):
                     button.click()
                     break
             sleep(1)
-            account_button_list = self._find_elements_by_id(
-                "com.android.vending:id/0_resource_name_obfuscated"
+            account_button_list = self.elements_selector(
+                "@com.android.vending:id/0_resource_name_obfuscated"
             )
             for button in account_button_list:
                 button_text = button.get_attribute("text")
@@ -203,7 +220,7 @@ class CSUser(User):
                     button.click()
                     break
             sleep(3)
-            device_account_list = self._find_elements_by_id("android:id/summary")
+            device_account_list = self.elements_selector("@android:id/summary")
             for account in device_account_list:
                 if (
                     account.get_attribute("text")
@@ -213,65 +230,70 @@ class CSUser(User):
                 else:
                     account.click()
                     sleep(1)
-                    self._find_element_by_id_and_click("com.android.settings:id/button")
-                    self._find_element_by_id_and_click("android:id/button1")
+                    self.element_selector("@com.android.settings:id/button").click()
+                    self.element_selector("@android:id/button1").click()
         except Exception:
             pass
 
     def facebook_sign_in(self, email, password):
         self.account_page()
-        self._find_element_by_xpath_and_click(ACCOUNTPAGE["sign_in_facebook"])
+        self.element_selector(ACCOUNTPAGE["sign_in_facebook"]).click()
         sleep(2)
         try:
-            self._find_element_by_id_and_click("com.android.chrome:id/terms_accept")
-            self._find_element_by_id_and_click("com.android.chrome:id/negative_button")
+            self.element_selector("com.android.chrome:id/terms_accept").click()
+            self.element_selector("com.android.chrome:id/negative_button").click()
         except NoSuchElementException:
             pass
         sleep(2)
         try:
-            self._find_element_by_xpath_and_send_keys(FACEBOOK_SIGNIN["email"], email)
+            self.element_selector(FACEBOOK_SIGNIN["email"]).send_keys(email)
         except NoSuchElementException:
-            self._find_element_by_xpath_and_click(FACEBOOK_SIGNIN["continue"])
+            self.element_selector(FACEBOOK_SIGNIN["continue"]).click()
             return
-        self._find_element_by_xpath_and_send_keys(FACEBOOK_SIGNIN["password"], password)
-        self._find_element_by_xpath_and_click(FACEBOOK_SIGNIN["enter"])
+        self.element_selector(FACEBOOK_SIGNIN["password"]).send_keys(password)
+        self.element_selector(FACEBOOK_SIGNIN["enter"]).click()
         sleep(2)
         try:
-            self._find_element_by_xpath(HOMEPAGE["account"])
+            self.element_selector(HOMEPAGE["account"])
         except NoSuchElementException:
-            self._find_element_by_xpath_and_click(FACEBOOK_SIGNIN["continue"])
+            self.element_selector(FACEBOOK_SIGNIN["continue"]).click()
 
     def line_sign_in(self, email, password):
         self.account_page()
-        self._find_element_by_xpath_and_click(ACCOUNTPAGE["sign_in_line"])
-        self._find_element_by_xpath_and_send_keys(LINE_SIGNIN["email"], email)
-        self._find_element_by_xpath_and_send_keys(LINE_SIGNIN["password"], password)
-        self._find_element_by_xpath_and_click(LINE_SIGNIN["enter"])
+        self.element_selector(ACCOUNTPAGE["sign_in_line"]).click()
+        sleep(2)
+        try:
+            self.element_selector("com.android.chrome:id/terms_accept").click()
+            self.element_selector("com.android.chrome:id/negative_button").click()
+        except NoSuchElementException:
+            pass
+        sleep(2)
+        self.element_selector(LINE_SIGNIN["email"]).send_keys(email)
+        self.element_selector(LINE_SIGNIN["password"]).send_keys(password)
+        self.element_selector(LINE_SIGNIN["enter"]).click()
         sleep(1)
-        self._find_element_by_xpath_and_click(LINE_SIGNIN["allow_access"])
+        self.element_selector(LINE_SIGNIN["allow_access"]).click()
 
     def google_sign_in(self, email, password):
         self.account_page()
-        self._find_element_by_xpath_and_click(ACCOUNTPAGE["sign_in_google"])
+        self.element_selector(ACCOUNTPAGE["sign_in_google"]).click()
         sleep(3)
         try:
-            self._find_element_by_xpath_and_send_keys(GOOGLE_SIGNIN["email"], email)
-            self._find_element_by_xpath_and_click(GOOGLE_SIGNIN["email_next"])
+            self.element_selector(GOOGLE_SIGNIN["email"]).send_keys(email)
+            self.element_selector(GOOGLE_SIGNIN["email_next"]).click()
             sleep(2)
-            self._find_element_by_xpath_and_send_keys(
-                GOOGLE_SIGNIN["password"], password
-            )
-            self._find_element_by_xpath_and_click(GOOGLE_SIGNIN["password_next"])
+            self.element_selector(GOOGLE_SIGNIN["password"]).send_keys(password)
+            self.element_selector(GOOGLE_SIGNIN["password_next"]).click()
             sleep(3)
-            self._find_element_by_xpath_and_click(GOOGLE_SIGNIN["agree_button"])
+            self.element_selector(GOOGLE_SIGNIN["agree_button"]).click()
             sleep(3)
-            switch = self._find_element_by_xpath(GOOGLE_SIGNIN["switch"])
+            switch = self.element_selector(GOOGLE_SIGNIN["switch"])
             if switch.text == "ON":
                 switch.click()
                 sleep(2)
             else:
                 pass
-            self._find_element_by_xpath_and_click(GOOGLE_SIGNIN["accept"])
+            self.element_selector(GOOGLE_SIGNIN["accept"]).click()
         except NoSuchElementException:
-            self._find_element_by_xpath_and_click(GOOGLE_SIGNIN["signed_account"])
+            self.element_selector(GOOGLE_SIGNIN["signed_account"]).click()
             pass
